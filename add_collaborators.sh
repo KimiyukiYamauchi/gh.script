2b30db66ba9ff051de3e61fd1ee9dbe4aea7532f
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-10-07 15:29:09 +0900

    collaboratorを追加するリポジトリの変更

--- add_collaborators.sh
+++ add_collaborators.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPO="KimiyukiYamauchi/prog1.py"  # 例: KimiyukiYamauchi/unix1.2025.y
+REPO="KimiyukiYamauchi/memo.react.tl"  # 例: KimiyukiYamauchi/unix1.2025.y
 PERMISSION="push"  # pull / push / admin
 
 while IFS= read -r user
--- remove_collaborators.sh
+++ remove_collaborators.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPO="KimiyukiYamauchi/prog1.py"  # 例: KimiyukiYamauchi/unix1.2025.y
+REPO="KimiyukiYamauchi/memo.react.tl"  # 例: KimiyukiYamauchi/unix1.2025.y
 
 # コラボレータ一覧を取得し、ログイン名だけ抽出してループ処理
 gh api "repos/$REPO/collaborators" \
